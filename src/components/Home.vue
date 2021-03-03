ba181c61491a16d0c48df8ef3c63139b306529e9
三生缘 <2366707867@qq.com>
2021-03-03 14:53:34 +0800

    完成了权限功能开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,50 +3,53 @@
     <!-- 头部区域 -->
     <el-header>
       <div>
-        <img src="../assets/heima.png"
-             alt="">
+        <img src="../assets/angel.gif" alt="" />
         <span>电商后台管理系统</span>
       </div>
-      <el-button type="info"
-                 @click="logout">退出</el-button>
+      <el-button type="info" @click="logout">退出</el-button>
     </el-header>
     <!-- 页面主体区域 -->
     <el-container>
       <!-- 侧边栏 -->
       <el-aside :width="isCollapse ? '64px' : '200px'">
-        <div class="toggle-button"
-             @click="toggleCollapse">|||</div>
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#333744"
-                 text-color="#fff"
-                 active-text-color="#409EFF"
-                 unique-opened
-                 :collapse="isCollapse"
-                 :collapse-transition="false"
-                 router
-                 :default-active="activePath">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+        >
           <!-- 一级菜单 -->
-          <el-submenu :index="item.id + ''"
-                      v-for="item in menulist"
-                      :key="item.id">
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menulist"
+            :key="item.id"
+          >
             <!-- 一级菜单的模板区域 -->
             <template slot="title">
               <!-- 图标 -->
               <i :class="iconsObj[item.id]"></i>
               <!-- 文本 -->
-              <span>{{item.authName}}</span>
+              <span>{{ item.authName }}</span>
             </template>
 
             <!-- 二级菜单 -->
-            <el-menu-item :index="'/' + subItem.path"
-                          v-for="subItem in item.children"
-                          :key="subItem.id"
-                          @click="saveNavState('/' + subItem.path)">
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
               <template slot="title">
                 <!-- 图标 -->
                 <i class="el-icon-menu"></i>
                 <!-- 文本 -->
-                <span>{{subItem.authName}}</span>
+                <span>{{ subItem.authName }}</span>
               </template>
             </el-menu-item>
           </el-submenu>
@@ -63,7 +66,7 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       // 左侧菜单数据
       menulist: [],
@@ -72,41 +75,41 @@ export default {
         103: 'iconfont icon-tijikongjian',
         101: 'iconfont icon-shangpin',
         102: 'iconfont icon-danju',
-        145: 'iconfont icon-baobiao'
+        145: 'iconfont icon-baobiao',
       },
       // 是否折叠
       isCollapse: false,
       // 被激活的链接地址
-      activePath: ''
+      activePath: '',
     }
   },
-  created () {
+  created() {
     this.getMenuList()
     this.activePath = window.sessionStorage.getItem('activePath')
   },
   methods: {
-    logout () {
+    logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
     },
     // 获取所有的菜单
-    async getMenuList () {
+    async getMenuList() {
       const { data: res } = await this.$http.get('menus')
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
       this.menulist = res.data
       // console.log(res)
     },
     // 点击按钮，切换菜单的折叠与展开
-    toggleCollapse () {
+    toggleCollapse() {
       this.isCollapse = !this.isCollapse
     },
     // 保存链接的激活状态
-    saveNavState (activePath) {
+    saveNavState(activePath) {
       window.sessionStorage.setItem('activePath', activePath)
       this.activePath = activePath
       console.log(this.$route)
-    }
-  }
+    },
+  },
 }
 </script>
 
