57a59919dc473adc7e8901bb7a2e847a4526528b
三生缘 <2366707867@qq.com>
2021-03-06 11:49:59 +0800

    params

---
+++ src/components/goods/List.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加商品</el-button>
+        </el-col>
+      </el-row>
+      <!-- table 表格 -->
+      <el-table :data="goodslist" border stripe>
+        <el-table-column label="#" type="index"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column
+          label="商品价格(元)"
+          prop="goods_price"
+          width="95px"
+        ></el-table-column>
+        <el-table-column
+          label="商品重量"
+          prop="goods_weight"
+          width="95px"
+        ></el-table-column>
+        <el-table-column
+          label="创建时间"
+          prop="add_time"
+          width="140px"
+        ></el-table-column>
+        <el-table-column label="操作" width="125px">
+          <template>
+            <el-button
+              size="mini"
+              type="primary"
+              icon="el-icon-edit"
+            ></el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              icon="el-icon-delete"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
